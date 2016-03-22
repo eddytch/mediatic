ab3a28b6fbb2d17c48cb09a1aa498f038de9061f
eddytch <eddy.tchouague@gmail.com>
2016-03-22 11:55:47 +0100

    Ajout class Adherent & Adresse

--- src/main/java/model/Adherent.java
+++ src/main/java/model/Adherent.java
@@ -27,4 +27,70 @@ public class Adherent {
 		this.montant = montant;
 	}
 
+	// Getters & Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDateNce() {
+		return dateNce;
+	}
+
+	public void setDateNce(Date dateNce) {
+		this.dateNce = dateNce;
+	}
+
+	public Date getDatePaiement() {
+		return datePaiement;
+	}
+
+	public void setDatePaiement(Date datePaiement) {
+		this.datePaiement = datePaiement;
+	}
+
+	public Date getDateFinCotisation() {
+		return dateFinCotisation;
+	}
+
+	public void setDateFinCotisation(Date dateFinCotisation) {
+		this.dateFinCotisation = dateFinCotisation;
+	}
+
+	public double getMontant() {
+		return montant;
+	}
+
+	public void setMontant(double montant) {
+		this.montant = montant;
+	}
+	// Fin Getters & Setters
+
 }
--- src/main/java/model/Adresse.java
+++ src/main/java/model/Adresse.java
@@ -1,5 +1,74 @@
 package model;
 
 public class Adresse {
+	
+	private Long id;
+	private String numRue;
+	private String nomRue;
+	private int cpVille;
+	private String ville;
+	private String pays;
+	
+	public Adresse() {
+		
+	}
+
+	public Adresse(String numRue, String nomRue, int cpVille, String ville, String pays) {
+		this.numRue = numRue;
+		this.nomRue = nomRue;
+		this.cpVille = cpVille;
+		this.ville = ville;
+		this.pays = pays;
+	}
+
+	// Getters & Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumRue() {
+		return numRue;
+	}
+
+	public void setNumRue(String numRue) {
+		this.numRue = numRue;
+	}
+
+	public String getNomRue() {
+		return nomRue;
+	}
+
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+
+	public int getCpVille() {
+		return cpVille;
+	}
+
+	public void setCpVille(int cpVille) {
+		this.cpVille = cpVille;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	// Fin Getters & Setters
 
 }
