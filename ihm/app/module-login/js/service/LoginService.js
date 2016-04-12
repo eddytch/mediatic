1aa1932a32e18c0d98adaa0f6b050019587345e2
eddytch <eddy.tchouague@gmail.com>
2016-04-12 17:14:55 +0200

    Correction login.js

--- ihm/app/module-login/js/service/LoginService.js
+++ ihm/app/module-login/js/service/LoginService.js
@@ -3,23 +3,36 @@
 //Récupération du module global pour y ajouter le service du Panier
 angular.module('ModuleLogin').service('LoginService', ['$http', function($http) {
 	
-	var self = this;
+	var ctrl = this;
 	
 	var url = "http://10.34.10.140:8080/resource/connexion.login";
-	var promise = undefined;
+	var connected = false;
 	
 	// Fonction pour voir si l'user est connecté
 	this.isConnected = function(){
-		
+		return connected;
 	}
 	
 	// Fonction qui permet de se connecter
 	this.connect = function(login, mdp){
-		if (promise == undefined) {
-			promise = $http.post(url, {'login' : login, 'mdp' : mdp}).then(function(response) {
-				return response.data;
-			})
-		}
+		return $http.post(url, {'login' : login, 'mdp' : mdp}).then(function(response) {
+			connected = true;
+			return response.data;
+		});
 	};
+    
+    this.getValidForm = function() {
+    	if ($scope.myFormLogin.$invalid) {
+    		console.warn('Erreur : formulaire invalide');
+		} else {
+			var item = ctrl.item;
+			$http.post(url, item).then(function(){
+				console.info('Données sauvegardées')
+			}, function(){
+				console.warn('Erreur dans la sauvegarde ...');
+			})
+			console.info('Formulaire valide ...')
+		}
+	}
 	
 }]);
