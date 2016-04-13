1171051ab87ed72a277b45458e029a933ede3081
dimitri lanoe <dimitrilanoe35@gmail.com>
2016-04-13 16:29:14 +0200

    ajout de la recherche pour smartphone et pour tout les champs

--- ihm/app/module-adherent/js/adherent.js
+++ ihm/app/module-adherent/js/adherent.js
@@ -1,1 +1,10 @@
 angular.module("ModuleAdherent",['ngRoute','angularUtils.directives.dirPagination']) ;
+// Configuration du module ModuleCommande
+// => Injection du Provider du service $route afin de configurer la route des commandes.
+angular.module('ModuleAdherent').config(function($routeProvider){
+	$routeProvider.when('/rechAdherent', {
+		templateUrl : './module-adherent/templates/rechercherAdherent.html',
+		controller : 'AdherentController',
+		controllerAs : 'adherentController'
+	});
+});
