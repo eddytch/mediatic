3fca3b6f693e9a13bef351db13f435f069223e28
dimitri lanoe <dimitrilanoe35@gmail.com>
2016-04-14 11:26:09 +0200

    integration adherent dans l'application

--- ihm/app/module-adherent/js/adherent.js
+++ ihm/app/module-adherent/js/adherent.js
@@ -2,7 +2,7 @@ angular.module("ModuleAdherent",['ngRoute','angularUtils.directives.dirPaginatio
 // Configuration du module ModuleCommande
 // => Injection du Provider du service $route afin de configurer la route des commandes.
 angular.module('ModuleAdherent').config(function($routeProvider){
-	$routeProvider.when('/rechAdherent', {
+	$routeProvider.when('/adherent', {
 		templateUrl : './module-adherent/templates/rechercherAdherent.html',
 		controller : 'AdherentController',
 		controllerAs : 'adherentController'
